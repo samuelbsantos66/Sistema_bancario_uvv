93bd2cbdb0f26133e3a5cf5e978927a98f91d76e
Samuel Barbosa Santos <125471851+samuelbsantos66@users.noreply.github.com>
2024-10-17 08:51:09 -0300

    Add files via upload

---
+++ SistemaBancario_uvv/SistemaBancario_uvv/model/Model.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario_uvv.SistemaBancario_uvv.model
+{
+    internal class Model
+    {
+
+        public class Cliente
+        {
+            public string nome { get; set; }
+            public string cpf { get; set; }
+
+            public string email {  get; set; }
+
+            public Conta conta;
+
+            public Cliente(string numeroConta)
+            {
+                NumeroConta = numeroConta;
+                Saldo = 0;
+            }
+
+            public void Depositar(decimal valor)
+            {
+                if (valor > 0)
+                {
+                    Saldo += valor;
+                }
+            }
+        }
+
+        public class Conta
+        {
+            public string id { get; set; }
+            public decimal Saldo { get; private set; }
+
+            public ContaBancaria(string numeroConta)
+            {
+                NumeroConta = numeroConta;
+                Saldo = 0;
+            }
+
+            public void Depositar(decimal valor)
+            {
+                if (valor > 0)
+                {
+                    Saldo += valor;
+                }
+            }
+        }
+
+    }
+}
